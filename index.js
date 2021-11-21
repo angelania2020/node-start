63d4bb42ba475c8c8ea503de5a65f06d2df5b84d
Angelina <angelania@ya.ru>
2021-11-21 16:13:32 +0200

    nodemone

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const server = http.createServer((req, res) => {
     res.write('<h1>Hello from NodeJS</h1>')
     res.write('<h2>Hello from NodeJS</h2>')
     res.write('<h3>Hello from NodeJS</h3>')
+    res.write('<p>Hello from NodeJS</p>')
+    res.write('<p>Hello from NodeJS</p>')
     res.end(`
         <div style="background: red; width: 200px; height: 200px;">
             <h1>Test</h1>
